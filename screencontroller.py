470b92ab47295ccd792b749abdee53adaf74e7c8
OffedulDev <82319578+OffedulDev@users.noreply.github.com>
2021-09-25 14:34:16 +0200

    this is working yey

--- screencontroller.py
+++ screencontroller.py
@@ -3,30 +3,94 @@ pygame.init()
 
 objects = []
 
-screen = pygame.display.set_mode((1920, 1080))
+class InstanceReference:
+    def __init__(self, instanceref, posX, posY, name=None):
+        self.instanceref = instanceref
+        self.posX = posX
+        self.posY = posY
+        self.name = name
+
+screen = pygame.display.set_mode((1280, 780))
+clock = pygame.time.Clock()
+last_edit = InstanceReference("NLE", 0, 0)
+
+
 
 def getScreen():
     return screen
 
+def fixedInfoDisplay():
+    for m in objects:
+        if isinstance(m, InstanceReference):
+            if m.name != None:
+                print(f"Object Name: '{m.name}' Position: {m.posX}, {m.posY}")
+
 def updateDisplay():
     pygame.display.flip()
+
+def makeFont(font_name="Verdana", font_size=14, text="Failed to pass text", bold=False, italic=False, color=(255, 255, 255), posX=0, posY=0, actual_bg=None, last_edit=last_edit):
+    temp_font = pygame.font.SysFont(font_name, font_size, bold, italic)
+    render_font = temp_font.render(str(text), 1, color)
+    fontClass = InstanceReference(render_font, posX, posY, f"Rendered Font: {text}")
+
+    if last_edit == fontClass:
+        objects.pop(objects.index(last_edit))
+        insert
+        objects.append(fontClass)
+        last_edit = fontClass
+        if isinstance(actual_bg, InstanceReference): insertBackground(actual_bg)
+        drawImage(fontClass)
+    else:      
+        objects.append(fontClass)
+        last_edit = fontClass
+        if isinstance(actual_bg, InstanceReference): insertBackground(actual_bg)
+        drawImage(fontClass)
 
 def loadImage(filePath):
     return pygame.image.load(filePath)
 
+def getObjectList():
+    return objects
+
 def addCaption(text="Invalid Text"):
     pygame.display.set_caption(str(text))
 
-def drawImage(imageClass):
-    instance_values = getInstanceValues(imageClass)
-    instance = instance_values[0]
-    posX = instance_values[1]
-    posY = instance_values[2]
+def drawImage(imageClass, last_edit=last_edit):
+    if imageClass.name != None:
+        print(imageClass.name)
 
-    
-    objects.append(imageClass)
-    screen.blit(instance, (posX, posY))
+    if last_edit == imageClass:
+        objects.pop(objects.index(last_edit))
+        instance_values = getInstanceValues(imageClass)
+        instance = instance_values[0]
+        posX = instance_values[1]
+        posY = instance_values[2]
+        
+        last_edit = imageClass
+        objects.append(imageClass)
+        screen.blit(instance, (posX, posY))
+        return imageClass
+    else:
+        instance_values = getInstanceValues(imageClass)
+        instance = instance_values[0]
+        posX = instance_values[1]
+        posY = instance_values[2]
+        
+        last_edit = imageClass
 
+        objects.append(imageClass)
+        screen.blit(instance, (posX, posY))
+        return last_edit
+
+def removeObject(index):
+    object_idx = objects[index]
+    objects.pop(object_idx)
+    updateDisplay()
+
+
+def appendObject(classObject):
+    if isinstance(classObject, InstanceReference):
+        objects.append(classObject)
 
 def setIcon(iconClass):
     instance_values = getInstanceValues(iconClass)
@@ -40,10 +104,13 @@ def insertBackground(background_instance):
     posX = instance_values[1]
     posY = instance_values[2]
 
+    #objects.append(background_instance)
+     
+
     screen.blit(instance, (posX, posY))
 
-def makeInstanceReference(instanceref, posX, posY):
-    return InstanceReference(instanceref, posX, posY)
+def makeInstanceReference(instanceref, posX, posY, name=None):
+    return InstanceReference(instanceref, posX, posY, name)
 
 def getInstanceValues(classToGet):
     values = []
@@ -56,8 +123,3 @@ def getInstanceValues(classToGet):
         return values
 
 
-class InstanceReference:
-    def __init__(self, instanceref, posX, posY):
-        self.instanceref = instanceref
-        self.posX = posX
-        self.posY = posY
